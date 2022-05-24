2518c5fea92ed396db51aaf0125694b8e952520c
Daniel Uboa <51877907+daniuboa@users.noreply.github.com>
2022-05-24 18:49:48 +0300

    Update main.h

--- main.h
+++ main.h
@@ -17,24 +17,17 @@ typedef struct printer
 	int (*fn)(va_list);
 } print_t;
 
-int print_char(va_list ap);
 int _putchar(char c);
-void _puts(char *str);
-int _strlen(char *s);
 int _printf(const char *format, ...);
-char *convert_to(char representation[], unsigned int num, int base);
-int function_manager(char c, va_list arg);
-int _switch(char c, va_list arg);
-int print_character(va_list arg);
-int print_sign(va_list arg, int base);
-int print_unsign(va_list arg, int base);
+int print_char(va_list ap);
 int print_string(va_list ap);
-int print_ptr(va_list arg);
-int print_rot13(va_list arg);
-int print_rev(va_list arg);
-int print_base16_upper_lower(va_list arg, char *representation);
-int loop_format(va_list arg, const char *string);
-int call_function_manager(char aux, va_list arg);
-int check_percent(int *flag, char aux);
+int print_space(va_list ap);
+int print_int(va_list ap);
+int print_unsigned(va_list ap);
+int print_octal(va_list ap);
+int print_digit(int num, int *count);
+int print_unsigned_digit(unsigned int num, int *count);
+int print_digit_octal(unsigned int num, int *count);
+int call_print_fn(char ch, va_list ap);
 
 #endif
